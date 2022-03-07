70a4b307d75877136ff6a86c09f652ce3faf8dc0
alexkeuda <99165748+alexkeuda@users.noreply.github.com>
2022-03-07 09:16:47 +0200

    Tein kaikki muutokset koodeihin jotta localStorage tallentaa nimet

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
     if (kirjautunut === 'kylla'){
-    document.getElementById('tervetulo_teksti').textContent += ' Alex!';
+    document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
     }
 }
 
 function kirjaudu(){
-    localStorage.setItem("nimi", "alex");
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
 }
