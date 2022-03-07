9123bc13f2b93bdeaf8488a1581ed003d305a3a4
alexkeuda <99165748+alexkeuda@users.noreply.github.com>
2022-03-07 09:01:09 +0200

    Huomasin virheen nimessä

--- demo6/kirjautuminen.js
+++ demo6/kirjautuminen.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
 if (kirjautunut === 'kylla'){
-    document.getElementById('tervetulo_teksti').textContent += ' Otto!';
+    document.getElementById('tervetulo_teksti').textContent += ' Alex!';
 }
 }
