82d98c2e19fafd8166d554613087f65c0c08d1cd
alexkeuda <99165748+alexkeuda@users.noreply.github.com>
2022-03-08 08:51:51 +0200

    Tein uudelleen koodin js ja html

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 function onkoKirjautunut(){
+    document.getElementById("kirjaudu_ulos").style.display = "hide";
     if (localStorage.getItem("kirjautunut") === "kylla"){
-    document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
-    document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("tervetuloa_teksti").textContent += " " + localStorage.getItem("nimi");
+        document.getElementById("kirjautumis_lomake").style.display = "none"
     }
 }
 
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
+    document.getElementById("kirjaudu_ulos").style.display = "show";
 }
 
-function ulos() {
-    localStorage.clear();
+function ulos(){
+    localStorage.setItem("kirjautunut", "ei");
+        localStorage.removeItem();
 }
